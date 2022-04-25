1208687336f439d03709bd0f087c6217dcdc7f2b
noormohammad20 <mdsaim204@gmail.com>
2022-04-25 21:17:21 +0600

    added hero api

---
+++ __server_deploy.js
@@ -0,0 +1,27 @@
+/**
+ * ----------------------------
+ *   ONE TIME FOR EACH COMPUTER
+ * ----------------------------
+ * 1. create heroku account 
+ * 2. verify email 
+ * 3. install heroku cli
+ * 4. heroku login (in CMD)
+ * -----------------------------
+ * for each project one time
+ * ----------------------------
+ * 1. heroku create
+ * 2. make sure you : git add . git commit  and git push 
+ * 3. git push heroku main 
+ * 4. Go to Heroku Dashboard > Current Project > Setting > Revel Config Vars
+ * 5. Copy Paste Config Vars From Your .env file
+ * 6. Make sure you have whitelisted all id address to access mongodb 
+ * -------------------
+ *  UPDATE SERVER with new changes
+ * --------------------
+ * 1. Make Changes 
+ * 2. make sure you : git add . git commit  and git push 
+ * 3. git push heroku main 
+ * ----------------
+ * Connect Server with Client
+ * 
+*/
--- index.js
+++ index.js
@@ -109,6 +109,9 @@ run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('running server')
 })
+app.get('/hero', (req, res) => {
+    res.send('Programing Hero')
+})
 
 app.listen(port, () => {
     console.log('listening to port', port)
